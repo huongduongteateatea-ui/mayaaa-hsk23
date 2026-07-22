d22124a0a785b49490a8ed93531d323d2ec26050
huongduongteateatea-ui <huongduongteateatea@gmail.com>
2026-07-22 14:42:55 +0700

    add HSK4 lesson1 practice

---
+++ app/data/practice/hsk/hsk4/lesson1.ts
@@ -0,0 +1,1305 @@
+const lesson1 = {
+
+title:"HSK4 Bài 1 - 简单的爱情",
+
+
+quiz:[
+{
+id:1,
+question:"法律 nghĩa là gì?",
+options:[
+"Pháp luật",
+"Kinh nghiệm",
+"Tình yêu",
+"Cuộc sống"
+],
+answer:0
+},
+
+{
+id:2,
+question:"俩 nghĩa là gì?",
+options:[
+"Hai người",
+"Hai ngày",
+"Hai lần",
+"Hai cái"
+],
+answer:0
+},
+
+{
+id:3,
+question:"印象 nghĩa là gì?",
+options:[
+"Ấn tượng",
+"Ký ức",
+"Tính cách",
+"Thói quen"
+],
+answer:0
+},
+
+{
+id:4,
+question:"深 nghĩa là gì?",
+options:[
+"Sâu",
+"Cao",
+"Dài",
+"Rộng"
+],
+answer:0
+},
+
+{
+id:5,
+question:"熟悉 nghĩa là gì?",
+options:[
+"Quen thuộc",
+"Xa lạ",
+"Khó hiểu",
+"Quan trọng"
+],
+answer:0
+},
+
+{
+id:6,
+question:"不仅 nghĩa là gì?",
+options:[
+"Không chỉ",
+"Nếu",
+"Bởi vì",
+"Cho nên"
+],
+answer:0
+},
+
+{
+id:7,
+question:"性格 nghĩa là gì?",
+options:[
+"Tính cách",
+"Ngoại hình",
+"Sở thích",
+"Công việc"
+],
+answer:0
+},
+
+{
+id:8,
+question:"开玩笑 nghĩa là gì?",
+options:[
+"Nói đùa",
+"Cãi nhau",
+"Giải thích",
+"Xin lỗi"
+],
+answer:0
+},
+
+{
+id:9,
+question:"从来 nghĩa là gì?",
+options:[
+"Từ trước đến nay",
+"Hiện tại",
+"Sau này",
+"Đôi khi"
+],
+answer:0
+},
+
+{
+id:10,
+question:"最好 nghĩa là gì?",
+options:[
+"Tốt nhất",
+"Khá tốt",
+"Không tốt",
+"Tốt hơn"
+],
+answer:0
+},
+
+{
+id:11,
+question:"共同 nghĩa là gì?",
+options:[
+"Chung",
+"Khác",
+"Mới",
+"Cũ"
+],
+answer:0
+},
+
+{
+id:12,
+question:"适合 nghĩa là gì?",
+options:[
+"Phù hợp",
+"Giống",
+"Khác",
+"Thích"
+],
+answer:0
+},
+
+{
+id:13,
+question:"幸福 nghĩa là gì?",
+options:[
+"Hạnh phúc",
+"May mắn",
+"Thành công",
+"Vui vẻ"
+],
+answer:0
+},
+
+{
+id:14,
+question:"生活 nghĩa là gì?",
+options:[
+"Cuộc sống",
+"Công việc",
+"Gia đình",
+"Xã hội"
+],
+answer:0
+},
+
+{
+id:15,
+question:"刚 nghĩa là gì?",
+options:[
+"Vừa mới",
+"Đang",
+"Đã",
+"Sẽ"
+],
+answer:0
+},
+
+{
+id:16,
+question:"浪漫 nghĩa là gì?",
+options:[
+"Lãng mạn",
+"Hài hước",
+"Nghiêm túc",
+"Tự nhiên"
+],
+answer:0
+},
+
+{
+id:17,
+question:"够 nghĩa là gì?",
+options:[
+"Đủ",
+"Thiếu",
+"Nhiều",
+"Ít"
+],
+answer:0
+},
+
+{
+id:18,
+question:"缺点 nghĩa là gì?",
+options:[
+"Khuyết điểm",
+"Ưu điểm",
+"Đặc điểm",
+"Điểm số"
+],
+answer:0
+},
+
+{
+id:19,
+question:"接受 nghĩa là gì?",
+options:[
+"Chấp nhận",
+"Từ chối",
+"Thay đổi",
+"Giải quyết"
+],
+answer:0
+},
+
+{
+id:20,
+question:"羡慕 nghĩa là gì?",
+options:[
+"Ngưỡng mộ",
+"Ghen ghét",
+"Sợ hãi",
+"Lo lắng"
+],
+answer:0
+},
+
+{
+id:21,
+question:"爱情 nghĩa là gì?",
+options:[
+"Tình yêu",
+"Tình bạn",
+"Gia đình",
+"Hôn nhân"
+],
+answer:0
+},
+
+{
+id:22,
+question:"星星 nghĩa là gì?",
+options:[
+"Ngôi sao",
+"Mặt trời",
+"Mặt trăng",
+"Bầu trời"
+],
+answer:0
+},
+
+{
+id:23,
+question:"即使 nghĩa là gì?",
+options:[
+"Cho dù",
+"Nếu",
+"Vì",
+"Ngoài ra"
+],
+answer:0
+},
+
+{
+id:24,
+question:"加班 nghĩa là gì?",
+options:[
+"Tăng ca",
+"Nghỉ phép",
+"Tan làm",
+"Làm việc nhà"
+],
+answer:0
+},
+
+{
+id:25,
+question:"亮 nghĩa là gì?",
+options:[
+"Sáng",
+"Tối",
+"Cao",
+"Nặng"
+],
+answer:0
+},
+
+{
+id:26,
+question:"感动 nghĩa là gì?",
+options:[
+"Cảm động",
+"Cảm giác",
+"Cảm thấy",
+"Cảm ơn"
+],
+answer:0
+},
+
+{
+id:27,
+question:"自然 nghĩa là gì?",
+options:[
+"Tự nhiên",
+"Đương nhiên",
+"Tự do",
+"Bình thường"
+],
+answer:0
+},
+
+{
+id:28,
+question:"原因 nghĩa là gì?",
+options:[
+"Nguyên nhân",
+"Kết quả",
+"Lý do",
+"Cách làm"
+],
+answer:0
+},
+
+{
+id:29,
+question:"互相 nghĩa là gì?",
+options:[
+"Lẫn nhau",
+"Cùng nhau",
+"Một mình",
+"Thường xuyên"
+],
+answer:0
+},
+
+{
+id:30,
+question:"吸引 nghĩa là gì?",
+options:[
+"Thu hút",
+"Chấp nhận",
+"Hiểu",
+"Giúp đỡ"
+],
+answer:0
+},
+{
+id:31,
+question:"幽默 nghĩa là gì?",
+options:[
+"Hài hước",
+"Lãng mạn",
+"Nghiêm túc",
+"Thông minh"
+],
+answer:0
+},
+
+{
+id:32,
+question:"脾气 nghĩa là gì?",
+options:[
+"Tính khí",
+"Tính cách",
+"Sở thích",
+"Thói quen"
+],
+answer:0
+},
+
+{
+id:33,
+question:"温柔 nghĩa là gì?",
+options:[
+"Dịu dàng",
+"Mạnh mẽ",
+"Nghiêm khắc",
+"Nóng tính"
+],
+answer:0
+},
+
+{
+id:34,
+question:"外向 nghĩa là gì?",
+options:[
+"Hướng ngoại",
+"Hướng nội",
+"Năng động",
+"Yên tĩnh"
+],
+answer:0
+},
+
+{
+id:35,
+question:"内向 nghĩa là gì?",
+options:[
+"Hướng nội",
+"Hướng ngoại",
+"Vui vẻ",
+"Thoải mái"
+],
+answer:0
+},
+
+{
+id:36,
+question:"诚实 nghĩa là gì?",
+options:[
+"Thành thật",
+"Thông minh",
+"Chăm chỉ",
+"Dũng cảm"
+],
+answer:0
+},
+
+{
+id:37,
+question:"聪明 nghĩa là gì?",
+options:[
+"Thông minh",
+"Chăm chỉ",
+"May mắn",
+"Vui vẻ"
+],
+answer:0
+},
+
+{
+id:38,
+question:"努力 nghĩa là gì?",
+options:[
+"Cố gắng",
+"Nghỉ ngơi",
+"Chấp nhận",
+"Thay đổi"
+],
+answer:0
+},
+
+{
+id:39,
+question:"关心 nghĩa là gì?",
+options:[
+"Quan tâm",
+"Tin tưởng",
+"Ngưỡng mộ",
+"Thu hút"
+],
+answer:0
+},
+
+{
+id:40,
+question:"照顾 nghĩa là gì?",
+options:[
+"Chăm sóc",
+"Giúp đỡ",
+"Giải thích",
+"Nhìn thấy"
+],
+answer:0
+},
+
+{
+id:41,
+question:"相信 nghĩa là gì?",
+options:[
+"Tin tưởng",
+"Lo lắng",
+"Chờ đợi",
+"Hy vọng"
+],
+answer:0
+},
+
+{
+id:42,
+question:"理解 nghĩa là gì?",
+options:[
+"Hiểu",
+"Nhớ",
+"Quên",
+"Thay đổi"
+],
+answer:0
+},
+
+{
+id:43,
+question:"原来 nghĩa là gì?",
+options:[
+"Hóa ra",
+"Ban đầu",
+"Hiện tại",
+"Sau này"
+],
+answer:0
+},
+
+{
+id:44,
+question:"后来 nghĩa là gì?",
+options:[
+"Sau này",
+"Trước đây",
+"Lúc đầu",
+"Hiện tại"
+],
+answer:0
+},
+
+{
+id:45,
+question:"发现 nghĩa là gì?",
+options:[
+"Phát hiện",
+"Quên",
+"Tìm kiếm",
+"Lựa chọn"
+],
+answer:0
+},
+
+{
+id:46,
+question:"认为 nghĩa là gì?",
+options:[
+"Cho rằng",
+"Nhớ",
+"Biết",
+"Nói"
+],
+answer:0
+},
+
+{
+id:47,
+question:"觉得 nghĩa là gì?",
+options:[
+"Cảm thấy",
+"Quyết định",
+"Giải thích",
+"Chấp nhận"
+],
+answer:0
+},
+
+{
+id:48,
+question:"了解 nghĩa là gì?",
+options:[
+"Tìm hiểu",
+"Quên",
+"Thay đổi",
+"Rời đi"
+],
+answer:0
+},
+
+{
+id:49,
+question:"关系 nghĩa là gì?",
+options:[
+"Quan hệ",
+"Nguyên nhân",
+"Kết quả",
+"Cơ hội"
+],
+answer:0
+},
+
+{
+id:50,
+question:"之间 nghĩa là gì?",
+options:[
+"Ở giữa",
+"Bên cạnh",
+"Phía trước",
+"Bên trong"
+],
+answer:0
+},
+{
+id:51,
+question:"他们认识很多年了，彼此非常______。",
+options:[
+"熟悉",
+"浪漫",
+"简单",
+"缺点"
+],
+answer:0
+},
+
+{
+id:52,
+question:"她的______很好，大家都喜欢和她聊天。",
+options:[
+"性格",
+"法律",
+"星星",
+"生活"
+],
+answer:0
+},
+
+{
+id:53,
+question:"爱情不一定要很______，平淡的生活也可以很幸福。",
+options:[
+"浪漫",
+"共同",
+"自然",
+"深"
+],
+answer:0
+},
+
+{
+id:54,
+question:"每个人都有自己的优点和______。",
+options:[
+"缺点",
+"原因",
+"印象",
+"机会"
+],
+answer:0
+},
+
+{
+id:55,
+question:"虽然他工作很忙，但是每天都会______家人。",
+options:[
+"关心",
+"吸引",
+"开玩笑",
+"羡慕"
+],
+answer:0
+},
+
+{
+id:56,
+question:"他们有很多______的兴趣爱好。",
+options:[
+"共同",
+"深",
+"最好",
+"够"
+],
+answer:0
+},
+
+{
+id:57,
+question:"这件衣服很适合你，你穿起来很漂亮。“适合”的意思是：",
+options:[
+"Phù hợp",
+"Thích",
+"Quan trọng",
+"Đẹp"
+],
+answer:0
+},
+
+{
+id:58,
+question:"我刚吃完饭。“刚”的意思是：",
+options:[
+"Vừa mới",
+"Đang",
+"Sắp",
+"Đã"
+],
+answer:0
+},
+
+{
+id:59,
+question:"他从来没有去过中国。“从来”的意思是：",
+options:[
+"Từ trước đến nay",
+"Ngày mai",
+"Đã từng",
+"Đột nhiên"
+],
+answer:0
+},
+
+{
+id:60,
+question:"你最好早点睡觉。“最好”的意思是：",
+options:[
+"Tốt nhất",
+"Không tốt",
+"Rất tốt",
+"Không cần"
+],
+answer:0
+},
+
+{
+id:61,
+question:"即使天气不好，我们也要去。“即使”表示：",
+options:[
+"Cho dù",
+"Bởi vì",
+"Cho nên",
+"Nếu"
+],
+answer:0
+},
+
+{
+id:62,
+question:"他为了完成工作，经常加班。“加班”的意思是：",
+options:[
+"工作时间以外继续工作",
+"休息",
+"换工作",
+"学习"
+],
+answer:0
+},
+
+{
+id:63,
+question:"晚上天空中的星星很亮。“亮”的意思是：",
+options:[
+"Sáng",
+"Tối",
+"Xa",
+"Cao"
+],
+answer:0
+},
+
+{
+id:64,
+question:"听了这个故事，我非常感动。“感动”的意思是：",
+options:[
+"Cảm động",
+"Vui vẻ",
+"Tức giận",
+"Lo lắng"
+],
+answer:0
+},
+
+{
+id:65,
+question:"你们应该互相帮助。“互相”的意思是：",
+options:[
+"Lẫn nhau",
+"Một mình",
+"Thường xuyên",
+"Nhanh chóng"
+],
+answer:0
+},
+
+{
+id:66,
+question:"为什么很多人喜欢他？因为他的幽默吸引了大家。“吸引”的意思是：",
+options:[
+"Thu hút",
+"Tránh",
+"Giúp",
+"Chấp nhận"
+],
+answer:0
+},
+
+{
+id:67,
+question:"他说话很有意思，经常让大家笑。他很______。",
+options:[
+"幽默",
+"缺点",
+"法律",
+"自然"
+],
+answer:0
+},
+
+{
+id:68,
+question:"选择男朋友时，性格是否合适很重要。“选择”的意思是：",
+options:[
+"Lựa chọn",
+"Thay đổi",
+"Tin tưởng",
+"Nhận"
+],
+answer:0
+},
+
+{
+id:69,
+question:"他们的爱情故事给我留下了很深的______。",
+options:[
+"印象",
+"生活",
+"脾气",
+"星星"
+],
+answer:0
+},
+
+{
+id:70,
+question:"好的朋友之间应该互相______。",
+options:[
+"理解",
+"浪漫",
+"加班",
+"开玩笑"
+],
+answer:0
+},
+
+{
+id:71,
+question:"我觉得他们两个很______。",
+options:[
+"适合",
+"法律",
+"星星",
+"缺点"
+],
+answer:0
+},
+
+{
+id:72,
+question:"幸福的______需要两个人一起努力。",
+options:[
+"生活",
+"原因",
+"脾气",
+"电影"
+],
+answer:0
+},
+
+{
+id:73,
+question:"她喜欢看电影，也喜欢听______。",
+options:[
+"音乐",
+"法律",
+"性格",
+"关系"
+],
+answer:0
+},
+
+{
+id:74,
+question:"每个人都有不同的______。",
+options:[
+"性格",
+"星星",
+"爱情",
+"电影"
+],
+answer:0
+},
+
+{
+id:75,
+question:"他是一个很诚实的人，我非常______他。",
+options:[
+"相信",
+"加班",
+"浪漫",
+"开玩笑"
+],
+answer:0
+},
+
+{
+id:76,
+question:"你不了解他，所以不能随便评价他的______。",
+options:[
+"性格",
+"法律",
+"星星",
+"爱情"
+],
+answer:0
+},
+
+{
+id:77,
+question:"他们第一次见面，就给我留下了好______。",
+options:[
+"印象",
+"缺点",
+"原因",
+"生活"
+],
+answer:0
+},
+
+{
+id:78,
+question:"两个人在一起，最重要的是互相______。",
+options:[
+"理解",
+"加班",
+"开玩笑",
+"羡慕"
+],
+answer:0
+},
+
+{
+id:79,
+question:"我很______他们的爱情。",
+options:[
+"羡慕",
+"熟悉",
+"接受",
+"发现"
+],
+answer:0
+},
+
+{
+id:80,
+question:"经过几年时间，他们终于有了幸福的______。",
+options:[
+"生活",
+"星星",
+"法律",
+"电影"
+],
+answer:0
+},
+
+{
+id:81,
+question:"他刚来中国，对这里还不太______。",
+options:[
+"熟悉",
+"浪漫",
+"共同",
+"深"
+],
+answer:0
+},
+
+{
+id:82,
+question:"你应该学会______别人的缺点。",
+options:[
+"接受",
+"吸引",
+"开玩笑",
+"加班"
+],
+answer:0
+},
+
+{
+id:83,
+question:"我不知道他为什么这么生气，想知道其中的______。",
+options:[
+"原因",
+"星星",
+"爱情",
+"生活"
+],
+answer:0
+},
+
+{
+id:84,
+question:"他们两个有很多______的话题。",
+options:[
+"共同",
+"缺点",
+"最好",
+"亮"
+],
+answer:0
+},
+
+{
+id:85,
+question:"爱情不是一个人的事情，而是两个人的______。",
+options:[
+"关系",
+"法律",
+"电影",
+"星星"
+],
+answer:0
+},
+
+{
+id:86,
+question:"他说话的时候总喜欢______。",
+options:[
+"开玩笑",
+"加班",
+"接受",
+"羡慕"
+],
+answer:0
+},
+
+{
+id:87,
+question:"她的笑容很容易______别人。",
+options:[
+"吸引",
+"理解",
+"接受",
+"决定"
+],
+answer:0
+},
+
+{
+id:88,
+question:"我______你一定会成功。",
+options:[
+"相信",
+"加班",
+"浪漫",
+"羡慕"
+],
+answer:0
+},
+
+{
+id:89,
+question:"他们之间的感情很______。",
+options:[
+"深",
+"亮",
+"够",
+"刚"
+],
+answer:0
+},
+
+{
+id:90,
+question:"时间不______，我们快走吧。",
+options:[
+"够",
+"深",
+"共同",
+"熟悉"
+],
+answer:0
+},
+
+{
+id:91,
+question:"她每天工作很努力，希望过更好的______。",
+options:[
+"生活",
+"法律",
+"星星",
+"电影"
+],
+answer:0
+},
+
+{
+id:92,
+question:"爱情中最重要的是互相信任和______。",
+options:[
+"理解",
+"加班",
+"开玩笑",
+"羡慕"
+],
+answer:0
+},
+
+{
+id:93,
+question:"他们认识很久了，对彼此非常______。",
+options:[
+"熟悉",
+"浪漫",
+"亮",
+"够"
+],
+answer:0
+},
+
+{
+id:94,
+question:"每个人都有自己的想法和______。",
+options:[
+"性格",
+"星星",
+"电影",
+"法律"
+],
+answer:0
+},
+
+{
+id:95,
+question:"她的男朋友很幽默，也很______。",
+options:[
+"温柔",
+"法律",
+"缺点",
+"原因"
+],
+answer:0
+},
+
+{
+id:96,
+question:"好的爱情应该让两个人都感到______。",
+options:[
+"幸福",
+"加班",
+"困难",
+"压力"
+],
+answer:0
+},
+
+{
+id:97,
+question:"虽然有缺点，但是我们还是应该学会______。",
+options:[
+"接受",
+"吸引",
+"选择",
+"开玩笑"
+],
+answer:0
+},
+
+{
+id:98,
+question:"他们的故事让我非常______。",
+options:[
+"感动",
+"加班",
+"熟悉",
+"法律"
+],
+answer:0
+},
+
+{
+id:99,
+question:"我认为真正的爱情需要互相帮助和______。",
+options:[
+"理解",
+"星星",
+"电影",
+"法律"
+],
+answer:0
+},
+
+{
+id:100,
+question:"这是我听过最浪漫的爱情故事。",
+options:[
+"浪漫",
+"法律",
+"缺点",
+"共同"
+],
+answer:0
+}
+
+],
+
+
+translateB:[
+{
+question:"Tôi nghĩ tình yêu thật sự cần có sự tin tưởng và thấu hiểu lẫn nhau.",
+answer:"我觉得真正的爱情需要互相信任和理解。",
+pinyin:"Wǒ juéde zhēnzhèng de àiqíng xūyào hùxiāng xìnrèn hé lǐjiě。"
+},
+
+{
+question:"Mặc dù anh ấy rất bận, nhưng anh ấy vẫn luôn quan tâm đến gia đình.",
+answer:"虽然他很忙，但是他一直很关心家人。",
+pinyin:"Suīrán tā hěn máng, dànshì tā yìzhí hěn guānxīn jiārén。"
+},
+
+{
+question:"Hai người họ có rất nhiều điểm chung nên rất phù hợp với nhau.",
+answer:"他们有很多共同点，所以很适合在一起。",
+pinyin:"Tāmen yǒu hěn duō gòngtóngdiǎn, suǒyǐ hěn shìhé zài yìqǐ。"
+},
+
+{
+question:"Mỗi người đều có ưu điểm và khuyết điểm của riêng mình.",
+answer:"每个人都有自己的优点和缺点。",
+pinyin:"Měi ge rén dōu yǒu zìjǐ de yōudiǎn hé quēdiǎn。"
+},
+
+{
+question:"Một tình yêu tốt cần hai người cùng cố gắng.",
+answer:"一段好的爱情需要两个人一起努力。",
+pinyin:"Yí duàn hǎo de àiqíng xūyào liǎng ge rén yìqǐ nǔlì。"
+}
+],
+
+
+translateC:[
+{
+question:"真正的爱情不是每天都浪漫，而是在困难的时候也愿意陪伴对方。",
+answer:"Tình yêu thật sự không phải ngày nào cũng lãng mạn, mà là khi khó khăn vẫn sẵn lòng ở bên đối phương.",
+pinyin:"Zhēnzhèng de àiqíng bú shì měitiān dōu làngmàn, ér shì zài kùnnan de shíhou yě yuànyì péibàn duìfāng。"
+},
+
+{
+question:"每个人都有自己的优点和缺点，我们应该学会接受别人。",
+answer:"Mỗi người đều có ưu điểm và khuyết điểm của riêng mình, chúng ta nên học cách chấp nhận người khác.",
+pinyin:"Měi ge rén dōu yǒu zìjǐ de yōudiǎn hé quēdiǎn, wǒmen yīnggāi xuéhuì jiēshòu biérén。"
+},
+
+{
+question:"虽然他们的性格不一样，但是他们之间的关系很好。",
+answer:"Mặc dù tính cách của họ không giống nhau, nhưng mối quan hệ giữa họ rất tốt.",
+pinyin:"Suīrán tāmen de xìnggé bù yíyàng, dànshì tāmen zhījiān de guānxi hěn hǎo。"
+},
+
+{
+question:"我发现一个人的性格会影响他的生活和爱情。",
+answer:"Tôi phát hiện tính cách của một người sẽ ảnh hưởng đến cuộc sống và tình yêu của họ.",
+pinyin:"Wǒ fāxiàn yí ge rén de xìnggé huì yǐngxiǎng tā de shēnghuó hé àiqíng。"
+},
+
+{
+question:"如果两个人互相信任和理解，就容易拥有幸福的生活。",
+answer:"Nếu hai người tin tưởng và thấu hiểu lẫn nhau thì dễ có một cuộc sống hạnh phúc.",
+pinyin:"Rúguǒ liǎng ge rén hùxiāng xìnrèn hé lǐjiě, jiù róngyì yǒngyǒu xìngfú de shēnghuó。"
+}
+],
+
+
+speaking:{
+
+
+topic:"请介绍你心目中的爱情",
+
+topicVN:"Giới thiệu tình yêu trong suy nghĩ của bạn",
+
+
+question:{
+chinese:"",
+pinyin:"",
+meaning:""
+},
+
+
+vocabulary:[],
+
+
+outline:[],
+
+
+sample:"",
+samplePinyin:"",
+translation:""
+
+}
+
+
+};
+
+
+export default lesson1;
---
+++ app/practice/hsk/hsk4/lesson1/page.tsx
@@ -0,0 +1,111 @@
+import Quiz from "@/app/components/Quiz";
+import TranslatePractice from "@/app/components/TranslatePractice";
+import lesson1 from "@/app/data/practice/hsk/hsk4/lesson1";
+
+
+export default function Lesson1Page(){
+
+return (
+
+<main className="
+min-h-screen
+bg-gradient-to-br
+from-green-50
+to-yellow-50
+px-6
+py-12
+">
+
+<div className="max-w-5xl mx-auto">
+
+<h1 className="
+text-4xl
+font-bold
+text-green-900
+">
+HSK4 Bài 1
+</h1>
+
+
+<p className="mt-3 text-gray-600">
+Ôn tập từ vựng và luyện tập
+</p>
+
+
+
+<section className="mt-10">
+
+<h2 className="
+text-2xl
+font-bold
+text-green-900
+mb-5
+">
+📝 Trắc nghiệm từ vựng (100 câu)
+</h2>
+
+
+<Quiz
+
+questions={lesson1.quiz}
+
+/>
+
+
+</section>
+
+
+
+<section className="mt-16">
+
+<h2 className="
+text-2xl
+font-bold
+text-green-900
+mb-5
+">
+✍️ Dịch câu
+</h2>
+
+
+<TranslatePractice
+
+items={lesson1.translateB}
+
+/>
+
+
+</section>
+
+
+
+<section className="mt-16">
+
+<h2 className="
+text-2xl
+font-bold
+text-green-900
+mb-5
+">
+📖 Dịch nâng cao
+</h2>
+
+
+<TranslatePractice
+
+items={lesson1.translateC}
+
+/>
+
+
+</section>
+
+
+
+</div>
+
+</main>
+
+)
+
+}
